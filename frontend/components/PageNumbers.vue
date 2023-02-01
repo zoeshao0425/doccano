9345325f51b7d6db102bdfc46fd6641042d07c18
Erin Li <lihuiying2002@outlook.com>
2023-02-01 01:07:30 -0800

    added page number display

---
+++ frontend/components/PageNumbers.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="page-numbers">
+      <div class="page-numbers-title">
+        <h3>Pages in which "pain" appears:</h3>
+      </div>
+      <v-list-item-group v-model="selected" mandatory class="page-numbers-container">
+        <v-list-item v-for="n in totalPages" :key="n" :class="{'pages':n}"
+          @click="$router.push(`/projects`)">
+          <v-list-item-content class="page-number-content">
+            <v-list-item-title class="page-number-text">
+              {{ n }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      isProjectAdmin: {
+        type: Boolean,
+        default: false,
+        required: true
+      },
+      project: {
+        type: Object,
+        default: () => { },
+        required: true
+      }
+    },
+  
+    data() {
+      return {
+        totalPages: 20
+      }
+    },
+  
+    computed: {
+      filteredItems() {
+        const items = [
+          {
+            id: 1,
+            text: "Pain",
+            link: '/newpage',
+            isVisible: true
+          },
+          {
+            id: 2,
+            text: "Anxiety",
+            link: '/',
+            isVisible: true
+          }
+  
+        ]
+        return items.filter((item) => item.isVisible)
+      }
+    },
+  
+  }
+  </script>
+  
+  <style scoped>
+  .nav-pills>li>a {
+    padding: 10px 15px;
+    color: #333;
+    text-decoration: none;
+  }
+
+  .page-numbers-title {
+    margin-bottom: 10px;
+  }
+
+  .page-numbers-container {
+    display: grid;
+    grid-template-columns: repeat(15, 36px);
+    row-gap: 10px;
+    list-style: none;
+  }
+
+  .page-number-content {
+    padding: 0 0;
+  }
+
+  .page-number-text {
+    color: rgb(25, 118, 210);
+    display: flex;
+    justify-content: center;
+    font-size: 16px;
+  }
+
+  .v-list-item {
+    padding: 0 0;
+    min-height: 36px;
+  }
+  </style>
+  
+  
--- frontend/components/newSideBar.vue
+++ frontend/components/newSideBar.vue
@@ -1,35 +1,63 @@
 <template>
-    <div class="sidebar">
-      <div class="sidebar-header">
-        <h3>Possible Diseases</h3>
-      </div>
-      <v-list-item-group v-model="selected" mandatory>
-        <v-list-item
-        v-for="item in items" :key="item.id"
-        @click="$router.push(localePath(`/projects/${$route.params.id}/${item.link}`))"
-       >
-       <v-list-item-content>
+  <div class="sidebar">
+    <div class="sidebar-header">
+      <h3>Possible Diseases</h3>
+    </div>
+    <v-list-item-group v-model="selected" mandatory>
+      <v-list-item v-for="(item, i) in filteredItems" :key="i"
+        @click="$router.push(`/projects/${$route.params.id}/${item.link}`)">
+        <v-list-item-content>
           <v-list-item-title>
             {{ item.text }}
           </v-list-item-title>
         </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </div>
-  </template>
+      </v-list-item>
+    </v-list-item-group>
+  </div>
+</template>
 
 <script>
-  export default {
-    data() {
-      return {
-        items: [
-          { id: 1, text: "Anxiety", link: "/intent-detection-and-slot-filling/newPage",},
-          { id: 2, text: "Pain", link: "/" },
-          { id: 3, text: "Something else", link: "/" }
-        ]
-      };
+export default {
+  props: {
+    isProjectAdmin: {
+      type: Boolean,
+      default: false,
+      required: true
+    },
+    project: {
+      type: Object,
+      default: () => { },
+      required: true
     }
-  };
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    filteredItems() {
+      const items = [
+        {
+          id: 1,
+          text: "Pain",
+          link: '/',
+          isVisible: true
+        },
+        {
+          id: 2,
+          text: "Anxiety",
+          link: '/',
+          isVisible: true
+        }
+
+      ]
+      return items.filter((item) => item.isVisible)
+    }
+  },
+
+}
 </script>
 
 <style scoped>
@@ -37,15 +65,18 @@
   background-color: #f5f5f5;
   padding: 20px;
   width: 200px;
+  
 }
+
 .sidebar-header h3 {
   margin-top: 0;
+  margin-bottom: 10px;
 }
+
 .nav-pills>li>a {
   padding: 10px 15px;
   color: #333;
   text-decoration: none;
 }
 </style>
-  
-  
+
