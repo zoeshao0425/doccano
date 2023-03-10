bfbf6d83ead41bd250556cb26982e0a3c8642d0c
Jonathan Becker <76632760+jbecker7@users.noreply.github.com>
2023-03-09 18:32:02 -0800

    Update useTeacherList.ts

--- frontend/composables/useTeacherList.ts
+++ frontend/composables/useTeacherList.ts
@@ -1,74 +1,67 @@
-import { reactive } from '@nuxtjs/composition-api'
-import { Category } from '~/domain/models/tasks/category'
+import { reactive } from '@nuxtjs/composition-api';
+import { Category } from '~/domain/models/tasks/category';
 
+// A hook that returns an object with functions to manage teacher list state
 export const useTeacherList = (repository: any) => {
-
-  /**
-   * Reactive state object containing the list of teachers.
-   */
+  // Define the reactive state
   const state = reactive({
     teacherList: []
-  })
+  });
 
-  /**
-   * Asynchronous function used to fetch the list of teachers from the server using the specified repository.
-   * @param projectId - The ID of the project to retrieve the list of teachers for.
-   * @param exampleId - The ID of the example to retrieve the list of teachers for.
-   */
+  // Fetches the teacher list from the repository and sets it in the state
   const getTeacherList = async (projectId: string, exampleId: number) => {
-    state.teacherList = await repository.list(projectId, exampleId)
-  }
+    state.teacherList = await repository.list(projectId, exampleId);
+  };
 
-  /**
-   * Asynchronous function used to remove a teacher from the list using the specified repository.
-   * @param projectId - The ID of the project to remove the teacher from.
-   * @param exampleId - The ID of the example to remove the teacher from.
-   * @param teacherId - The ID of the teacher to remove.
-   */
+  // Removes a teacher from the teacher list and updates the state
   const removeTeacher = async (projectId: string, exampleId: number, teacherId: number) => {
-    await repository.delete(projectId, exampleId, teacherId)
-    await getTeacherList(projectId, exampleId)
-  }
+    await repository.delete(projectId, exampleId, teacherId);
+    await getTeacherList(projectId, exampleId);
+  };
 
-  /**
-   * Asynchronous function used to annotate a label on an example using the specified repository.
-   * @param projectId - The ID of the project to annotate the label for.
-   * @param exampleId - The ID of the example to annotate the label for.
-   * @param labelId - The ID of the label to annotate.
-   */
+  // Creates a new label with the given labelId and updates the teacher list state
   const annotateLabel = async (projectId: string, exampleId: number, labelId: number) => {
-    const category = Category.create(labelId)
-    await repository.create(projectId, exampleId, category)
-    await getTeacherList(projectId, exampleId)
-  }
+    const category = Category.create(labelId);
+    await repository.create(projectId, exampleId, category);
+    await getTeacherList(projectId, exampleId);
+  };
 
-  /**
-   * Asynchronous function used to clear the list of teachers for an example using the specified repository.
-   * @param projectId - The ID of the project to clear the teacher list for.
-   * @param exampleId - The ID of the example to clear the teacher list for.
-   */
+  // Removes all teachers from the teacher list and updates the state
   const clearTeacherList = async (projectId: string, exampleId: number) => {
-    await repository.clear(projectId, exampleId)
-    await getTeacherList(projectId, exampleId)
-  }
+    await repository.clear(projectId, exampleId);
+    await getTeacherList(projectId, exampleId);
+  };
 
-  /**
-   * Asynchronous function used to automatically label an example using the specified repository.
-   * @param projectId - The ID of the project to auto-label the example for.
-   * @param exampleId - The ID of the example to auto-label.
-   */
+  // Automatically labels the teacher list and updates the state
   const autoLabel = async (projectId: string, exampleId: number) => {
-    await repository.autoLabel(projectId, exampleId)
-    await getTeacherList(projectId, exampleId)
-  }
+    await repository.autoLabel(projectId, exampleId);
+    await getTeacherList(projectId, exampleId);
+  };
 
-  /**
-   * Asynchronous function used to annotate or remove a label on an example using the specified repository.
-   * @param projectId - The ID of the project to annotate or remove the label for.
-   * @param exampleId - The ID of the example to annotate or remove the label for.
-   * @param srcKey - The source key of the label to annotate or remove.
-   */
+  // Either removes the label from the teacher list or adds it and updates the state
   const annotateOrRemoveLabel = async (projectId: string, exampleId: number, srcKey: string) => {
-    const labelId = parseInt(srcKey, 10)
+    const labelId = parseInt(srcKey, 10);
+    // Find the annotation with the given labelId
     // @ts-ignore
-    const annotation = state.teacherList.find((item)
+    const annotation = state.teacherList.find((item) => item.label === labelId);
+    if (annotation) {
+      // If the annotation exists, remove it from the teacher list
+      // @ts-ignore
+      await removeTeacher(projectId, exampleId, annotation.id);
+    } else {
+      // Otherwise, add the label to the teacher list
+      await annotateLabel(projectId, exampleId, labelId);
+    }
+  };
+
+  // Return an object with the state and functions to manage it
+  return {
+    state,
+    getTeacherList,
+    annotateLabel,
+    annotateOrRemoveLabel,
+    removeTeacher,
+    clearTeacherList,
+    autoLabel
+  };
+};
